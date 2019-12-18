1c7b58b7b3e274229a08ccacf94aacd4a0492bdd
Javier Castro <jaca2899@gmail.com>
2019-12-18 17:15:58 -0500

    Defined a letterbox function. This function evenly scales the game screen so that it fits snuggly within the main window.

--- window.c
+++ window.c
@@ -40,3 +40,52 @@ SDL_Surface* create_game_screen(int pixel_width, int pixel_height)
 
 	return surface;
 }
+
+/* This function takes an SDL Surface src, and scales it evenly so that it fits
+ * snuggly within the surface dst. This function has no return value, so it has
+ * to alter dst. */
+void letterbox(SDL_Surface *src, SDL_Surface *dst)
+{
+	double w_src = (double) src->w;
+	double h_src = (double) src->h;
+	double w_dst = (double) dst->w;
+	double h_dst = (double) dst->h;
+
+	double ratio_src = w_src / h_src;
+	double ratio_dst = w_dst / h_dst;
+
+	double w_new, h_new;
+	int x_new, y_new; // coordinates of the top-left corner of the surface
+
+	if (ratio_dst == ratio_src) {
+		w_new = w_dst;
+		h_new = h_dst;
+
+		x_new = 0;
+		y_new = 0;
+	} else if (ratio_dst < ratio_src) {
+		w_new = w_dst;
+		h_new = w_dst / ratio_src;
+
+		int delta = h_dst - h_new;
+		x_new = 0;
+		y_new = (int) round(delta / 2);
+	} else if (ratio_dst > ratio_src) {
+		w_new = h_dst * ratio_src;
+		h_new = h_dst;
+
+		int delta = w_dst - w_new;
+		x_new = (int) round(delta / 2);
+		y_new = 0;
+	}
+
+	SDL_Rect *rect;
+	rect->x = x_new;
+	rect->y = y_new;
+	rect->w = w_new;
+	rect->h = h_new;
+
+	/* Null, because we're selecting the entirety of src and placing it
+	 * within a portion of dst, defined by the rectangle rect. */
+	SDL_BlitScaled(src, NULL, dst, rect);
+}
