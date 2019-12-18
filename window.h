b70603c95fdeaa4fd2cbdc6f86fb67a5b752d58a
Javier Castro <jaca2899@gmail.com>
2019-12-18 17:58:42 -0500

    Added header guards to `window.h`.

--- window.h
+++ window.h
@@ -1,6 +1,11 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
 SDL_Window* open_window(const char *name);
 SDL_Surface* create_game_screen(int pixel_width, int pixel_height);
 void letterbox(SDL_Surface *src, SDL_Surface *dst);
+
+#endif
