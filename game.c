195b1bfaf3f7b731f84dec5763f4b7c2d8426647
Javier Castro <jaca2899@gmail.com>
2019-12-18 19:42:57 -0500

    Made it so that the window now closes when a close request is given.

--- game.c
+++ game.c
@@ -5,6 +5,22 @@
 #include "game.h"
 #include "window.h"
 
+/* If this function returns true, then the game loop continues. If it returns
+ * false, then the game loop is broken. */
+bool handle_event(SDL_Event event)
+{
+	bool loop_condition = true;
+	switch (event.type) {
+		case SDL_QUIT:
+			loop_condition = false;
+			break;
+		default:
+			break;
+	}
+
+	return loop_condition;
+}
+
 void play()
 {
 	SDL_Window *window = open_window("Test Window");
@@ -15,8 +31,15 @@ void play()
 	 * window. */
 	SDL_Surface *game_screen = create_game_screen(100, 100);
 
+	bool loop_condition = true;
+	while (loop_condition) {
 
-	while (true) {
+		/* Handle any events, like a close request, a key press, or a
+		 * key release. If a close request is given, then the game loop
+		 * is broken. */
+		SDL_Event event;
+		while (SDL_PollEvent(&event) != 0)
+			loop_condition = handle_event(event);
 
 		/* Overwrite every pixel in the window surface and in the game
 		 * screen with a solid color. */
--- game.h
+++ game.h
@@ -1,12 +1,14 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <stdbool.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
 #include "game.h"
 #include "window.h"
 
+bool handle_event(SDL_Event event);
 void play();
 
 #endif
