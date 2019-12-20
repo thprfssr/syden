6c8367eef68ea4f59bbc6d1175f189da008ed636
Javier Castro <jaca2899@gmail.com>
2019-12-20 13:02:08 -0500

    Moved all the macros from `game.h` to `config.h`.

---
+++ config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define MAIN_WINDOW_TITLE "Syden"
+#define GAME_SCREEN_WIDTH 256
+#define GAME_SCREEN_HEIGHT 144
+#define DESIRED_FPS 60
+
+#define TILE_ATLAS_PATH "resources/tile_atlas.png"
+
+#endif
--- game.h
+++ game.h
@@ -5,13 +5,6 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
-#define MAIN_WINDOW_TITLE "Syden"
-#define GAME_SCREEN_WIDTH 256
-#define GAME_SCREEN_HEIGHT 144
-#define DESIRED_FPS 60
-
-#define TILE_ATLAS_PATH "resources/tile_atlas.png"
-
 bool handle_event(SDL_Event event);
 void play();
 
