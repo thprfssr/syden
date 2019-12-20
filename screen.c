a88b6c20450cc11f345581c581cc03eb8215dfc8
Javier Castro <jaca2899@gmail.com>
2019-12-20 13:04:24 -0500

    Included some code in the `draw_game_screen()` function in the `screen.c` file. This code simply draws the tile atlas onto the game screen. This is just meant to serve as a demonstration, and it should be changed to something more useful later on.

--- screen.c
+++ screen.c
@@ -1,9 +1,15 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
+#include "config.h"
+#include "general.h"
+
 /* This function is tasked with calling all the necessary functions to draw
  * everything in the game screen. It is assumed that the surface has already
  * been overwritten with a solid color. */
 void draw_game_screen(SDL_Surface *screen)
 {
+	SDL_Surface *tile_atlas = load_resource(TILE_ATLAS_PATH);
+	SDL_BlitSurface(tile_atlas, NULL, screen, NULL);
+	SDL_FreeSurface(tile_atlas);
 }
