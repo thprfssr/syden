d4691dd15f9a4b1021422af72858d4f9bec6caf3
Javier Castro <jaca2899@gmail.com>
2019-12-20 13:27:18 -0500

    Changed the code in `draw_game_screen()` to test out the newly defined function `load_tile()`.

--- screen.c
+++ screen.c
@@ -3,6 +3,7 @@
 
 #include "config.h"
 #include "general.h"
+#include "tiles.h"
 
 /* This function is tasked with calling all the necessary functions to draw
  * everything in the game screen. It is assumed that the surface has already
@@ -10,6 +11,10 @@
 void draw_game_screen(SDL_Surface *screen)
 {
 	SDL_Surface *tile_atlas = load_resource(TILE_ATLAS_PATH);
-	SDL_BlitSurface(tile_atlas, NULL, screen, NULL);
+	SDL_Surface *tile = get_tile(tile_atlas, 0);
+
+	SDL_BlitSurface(tile, NULL, screen, NULL);
+
 	SDL_FreeSurface(tile_atlas);
+	SDL_FreeSurface(tile);
 }
