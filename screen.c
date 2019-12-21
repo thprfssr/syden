252a7fc9d06e566c7cf296c9fc3cbc387e7e3aa4
Javier Castro <jaca2899@gmail.com>
2019-12-20 22:34:33 -0500

    Wrote some code in `screen.c` that allows you to scroll through the region.

--- screen.c
+++ screen.c
@@ -1,9 +1,14 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
+#include "camera.h"
 #include "config.h"
+#include "controls.h"
 #include "general.h"
 #include "tiles.h"
+
+int X = 0;
+int Y = 0;
 
 /* This function is tasked with calling all the necessary functions to draw
  * everything in the game screen. It is assumed that the surface has already
@@ -14,9 +19,19 @@ void draw_game_screen(SDL_Surface *screen)
 	SDL_Surface *tile_atlas = load_resource(TILE_ATLAS_PATH);
 	char *map_csv = read_file("resources/test_map.csv");
 
+	int delta = 2;
+	if (is_button_pressed(BUTTON_UP))
+		Y -= delta;
+	if (is_button_pressed(BUTTON_DOWN))
+		Y += delta;
+	if (is_button_pressed(BUTTON_LEFT))
+		X -= delta;
+	if (is_button_pressed(BUTTON_RIGHT))
+		X += delta;
+
 	/* Copy the region onto the screen. */
 	SDL_Surface *region = draw_region(tile_atlas, map_csv);
-	SDL_BlitSurface(region, NULL, screen, NULL);
+	camera_view(region, screen, X, Y);
 
 	/* Free resources. */
 	SDL_FreeSurface(region);
