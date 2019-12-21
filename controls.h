e958ed672cef55c9686fc92f21c6e3ad835fe8e3
Javier Castro <jaca2899@gmail.com>
2019-12-20 19:51:05 -0500

    Created a file `controls.h`, and defined several macros which correspond to the various buttons in our abstract controller. Moreover, I defined a global variable called `CONTROLLER_STATUS` which holds all the information about which buttons are pressed.

---
+++ controls.h
@@ -0,0 +1,23 @@
+#ifndef CONTROLS_H
+#define CONTROLS_H
+
+#define BUTTON_UP	(1 << 0)
+#define BUTTON_DOWN	(1 << 1)
+#define BUTTON_LEFT	(1 << 2)
+#define BUTTON_RIGHT	(1 << 3)
+#define BUTTON_START	(1 << 4)
+#define BUTTON_SELECT	(1 << 5)
+#define BUTTON_A	(1 << 6)
+#define BUTTON_B	(1 << 7)
+#define BUTTON_X	(1 << 8)
+#define BUTTON_Y	(1 << 9)
+#define BUTTON_L	(1 << 10)
+#define BUTTON_R	(1 << 11)
+
+int CONTROLLER_STATUS = 0;
+
+
+
+
+
+#endif
