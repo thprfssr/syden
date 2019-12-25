55cd7861f221c1680c80c97256d0944e299e3182
Javier Castro <jaca2899@gmail.com>
2019-12-25 00:17:35 -0500

    Added a function which allows one to move the camera position.

--- camera.c
+++ camera.c
@@ -1,8 +1,14 @@
+#include <math.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
 #include "camera.h"
+#include "general.h"
+
+double CAMERA_POSITION_X = 0;
+double CAMERA_POSITION_Y = 0;
+
 
 /* This function takes a large surface `src`, and draws it onto `dst`. However,
  * it only draws as much of `src` as fits in `dst`. The coordinates passed to
@@ -36,4 +42,52 @@ bool camera_collision(SDL_Surface *src, int x_center, int y_center)
 	int y = y_center;
 
 	return (x - w < 0) || (y - h < 0) || (w < x + w) || (h < y + h);
+}
+
+void move_camera(int direction, double magnitude)
+{
+	switch(direction) {
+		case (NORTH):
+			CAMERA_POSITION_Y -= magnitude;
+			break;
+		case (SOUTH):
+			CAMERA_POSITION_Y += magnitude;
+			break;
+		case (EAST):
+			CAMERA_POSITION_X += magnitude;
+			break;
+		case (WEST):
+			CAMERA_POSITION_Y -= magnitude;
+			break;
+		case (NORTH | EAST):
+			CAMERA_POSITION_X += magnitude / sqrt(2);
+			CAMERA_POSITION_Y -= magnitude / sqrt(2);
+			break;
+		case (NORTH | WEST):
+			CAMERA_POSITION_X -= magnitude / sqrt(2);
+			CAMERA_POSITION_Y -= magnitude / sqrt(2);
+			break;
+		case (SOUTH | EAST):
+			CAMERA_POSITION_X += magnitude / sqrt(2);
+			CAMERA_POSITION_Y += magnitude / sqrt(2);
+			break;
+		case (SOUTH | WEST):
+			CAMERA_POSITION_X -= magnitude / sqrt(2);
+			CAMERA_POSITION_Y += magnitude / sqrt(2);
+			break;
+		case (NORTH | SOUTH | EAST):
+			CAMERA_POSITION_X += magnitude / sqrt(2);
+			break;
+		case (NORTH | SOUTH | WEST):
+			CAMERA_POSITION_X -= magnitude / sqrt(2);
+			break;
+		case (EAST | WEST | NORTH):
+			CAMERA_POSITION_Y -= magnitude / sqrt(2);
+			break;
+		case (EAST | WEST | SOUTH):
+			CAMERA_POSITION_Y += magnitude / sqrt(2);
+			break;
+		default:
+			break;
+	}
 }
--- camera.h
+++ camera.h
@@ -5,14 +5,10 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
-/* This function takes a large surface `src`, and draws it onto `dst`. However,
- * it only draws as much of `src` as fits in `dst`. The coordinates passed to
- * this function simply correspond to the point within `src` at which you wish
- * the view to be centered. */
-void camera_view(SDL_Surface *src, SDL_Surface *dst, int x_center, int y_center);
+double CAMERA_POSITION_X;
+double CAMERA_POSITION_Y;
 
-/* This function checks whether the camera view goes beyond the boundaries of
- * a given surface. */
+void camera_view(SDL_Surface *src, SDL_Surface *dst, int x_center, int y_center);
 bool camera_collision(SDL_Surface *src, int x_center, int y_center);
 
 #endif
