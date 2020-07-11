66f58eb6c132c5dea2f636d21cd26f708cc17bcf
Javier Castro <jaca2899@gmail.com>
2020-07-11 16:16:03 -0400

    Added an integer to the `Animation` struct, which will be used in the future to hold the current animation frame.

--- animation.c
+++ animation.c
@@ -4,21 +4,21 @@
 #include "general.h"
 
 
-struct Animation LINK_WALKING_SOUTH = {NULL, 10, 32, 32,
+struct Animation LINK_WALKING_SOUTH = {NULL, 10, 32, 32, 0,
 	"resources/link_walking_south.png"};
-struct Animation LINK_WALKING_NORTH = {NULL, 10, 32, 32,
+struct Animation LINK_WALKING_NORTH = {NULL, 10, 32, 32, 0,
 	"resources/link_walking_north.png"};
-struct Animation LINK_WALKING_WEST = {NULL, 10, 32, 32,
+struct Animation LINK_WALKING_WEST = {NULL, 10, 32, 32, 0,
 	"resources/link_walking_west.png"};
-struct Animation LINK_WALKING_EAST = {NULL, 10, 32, 32,
+struct Animation LINK_WALKING_EAST = {NULL, 10, 32, 32, 0,
 	"resources/link_walking_east.png"};
-struct Animation LINK_IDLE_SOUTH = {NULL, 1, 32, 32,
+struct Animation LINK_IDLE_SOUTH = {NULL, 1, 32, 32, 0,
 	"resources/link_idle_south.png"};
-struct Animation LINK_IDLE_NORTH = {NULL, 1, 32, 32,
+struct Animation LINK_IDLE_NORTH = {NULL, 1, 32, 32, 0,
 	"resources/link_idle_north.png"};
-struct Animation LINK_IDLE_WEST = {NULL, 1, 32, 32,
+struct Animation LINK_IDLE_WEST = {NULL, 1, 32, 32, 0,
 	"resources/link_idle_west.png"};
-struct Animation LINK_IDLE_EAST = {NULL, 1, 32, 32,
+struct Animation LINK_IDLE_EAST = {NULL, 1, 32, 32, 0,
 	"resources/link_idle_east.png"};
 
 SDL_Surface* get_animation_frame(struct Animation animation, int index)
